91278574fec3ead42a80a35ba8a180e1d1bf7a0b
Pratyush Prakhar <pratyush.prakhar21@gmail.com>
2023-08-11 10:32:59 +0530

    Project-3
    Marvel Quiz mini-project

---
+++ NeoG_Mini-Projects/Marvel-Quiz-App/script.js
@@ -0,0 +1,113 @@
+const questions = [
+    {
+        question: "Q1. How many Infinity Stones are there?",
+        options: ["Five", "Six", "Seven", "Nine"],
+        correct: 1
+    },
+    {
+        question: "Q2. Where is Captain America from?",
+        options: ["India", "USA", "Brooklyn", "Canada"],
+        correct: 2
+    },
+    {
+        question: "Q3. Who is Tony Stark's father?",
+        options: ["Howard Stark", "Tony Stark Sr.", "Edwin Jarvis", "Bruce Wayne"],
+        correct: 0
+    },
+    {
+        question: "Q4. What is the name of the character known as Black Widow?",
+        options: ["Natasha Romanoff", "Wanda Maximoff", "Carol Danvers", "Jessica Drew"],
+        correct: 0
+    },
+    {
+        question: "Q5. What type of doctor is Doctor Strange?",
+        options: ["Neurosurgeon", "Cardiologist", "Radiologist", "Orthopedic Surgeon"],
+        correct: 0
+    }
+];
+
+
+const questionElement = document.querySelector(".question");
+const optionsElements = document.querySelectorAll(".option");
+const scoreElement = document.querySelector(".score");
+const skipButton = document.querySelector(".skip-button");
+const resultElement = document.querySelector(".result");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function showQuestion(index) {
+    questionElement.textContent = questions[index].question;
+    optionsElements.forEach((option, i) => {
+        option.textContent = questions[index].options[i];
+    });
+}
+
+function updateScore() {
+    scoreElement.textContent = `${score}/${questions.length}`;
+}
+
+function showResult() {
+    resultElement.textContent = getResultMessage(score);
+    retryButton.style.display = "block"; 
+    skipButton.style.display = "none"; 
+}
+
+function getResultMessage(score) {
+    if (score === 0) {
+        return "Oh no! Keep trying!";
+    } else if (score <= 2) {
+        return "Not bad! Keep practicing!";
+    } else if (score <= 4) {
+        return "Great job! You're almost there!";
+    } else {
+        return "Marvel Mastery Unleashed!";
+    }
+}
+
+showQuestion(currentQuestionIndex);
+updateScore();
+
+// Event listener for option buttons
+optionsElements.forEach((option, index) => {
+    option.addEventListener("click", () => {
+        if (index === questions[currentQuestionIndex].correct) {
+            score++;
+            updateScore();
+        }
+
+        // Move to the next question
+        currentQuestionIndex++;
+        if (currentQuestionIndex < questions.length) {
+            showQuestion(currentQuestionIndex);
+            resultElement.textContent = ""; // Clear result message
+        } else {
+            showResult();
+        }
+    });
+});
+
+skipButton.addEventListener("click", () => {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion(currentQuestionIndex);
+    } else {
+        showResult();
+    }
+});
+
+const retryButton = document.querySelector(".retry-button");
+
+// Function to reset the quiz
+function resetQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    showQuestion(currentQuestionIndex);
+    updateScore();
+    resultElement.textContent = ""; // Clear result message
+    retryButton.style.display = "none"; // Hide retry button
+    skipButton.style.display = "block"; 
+}
+
+// Event listener for the Retry button
+retryButton.addEventListener("click", resetQuiz);
