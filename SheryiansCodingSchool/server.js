d25bc1f83ce251cf46753b7755507a9d4c0f840f
Pratyush Prakhar <pratyush.prakhar21@gmail.com>
2023-10-13 19:23:55 +0530

    Animated_Landing_Page_with_Shery.js_library

---
+++ SheryiansCodingSchool/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+     
